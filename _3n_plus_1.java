8b03e277b9ed702a51fae02ea102b04e1ab0f4f5
fiflizc <1379308425@qq.com>
2025-07-26 11:38:34 +0800

    Update _3n_plus_1.java
    remove the ultra

--- _3n_plus_1.java
+++ _3n_plus_1.java
@@ -21,21 +21,28 @@ public class _3n_plus_1 {
                 msSleep(2000);
                 System.out.println("What are you talking about?!");
                 System.out.println("There is no ultra!!!!!");
+		msSleep(3000)
+		System.out.println("Hello...I'm  ...ultra");
+		for(int i = 0;i <= 300;i++){
+		    System.out.println("GO BACK")
+		    msSleep(50)
+		}
+		break;
             }else{
                 System.out.println("The system is loading. Please wait a minute.");
                 msSleep(10000);
                 System.out.println("System: What are you talking about??!!! Retry!!!");
             }
-			msSleep(1000);
+		msSleep(1000);
             String re = strInput("Retry?(yes/no)");
             if (re.equals("yes")){
-			}else if (re.equals("no")){
-				break;
-			}
-			System.out.println("You must want to retry!");
-			msSleep(1000);
+		}else if (re.equals("no")){
+		break;
+		}
+		System.out.println("You must want to retry!");
+		msSleep(1000);
         } while (true);
         System.out.println("quit");
         msSleep(2000);
-	}
+    }
 }
